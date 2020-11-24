603898cf9c63a5a9626360f0c269a17c532fc15d
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2020-11-24 12:10:11 -0700

    v3

--- bin2tif_ps2.py
+++ bin2tif_ps2.py
@@ -12,6 +12,7 @@ import datetime
 import logging
 from typing import Optional
 import numpy as np
+from terrautils.spatial import scanalyzer_to_latlon
 import json
 import glob
 
@@ -40,6 +41,13 @@ def get_args():
                         type=str,
                         required=True)
 
+    parser.add_argument('-z',
+                        '--zoffset',
+                        help='Z-axis offset',
+                        metavar='z-offset',
+                        type=int,
+                        default=0.76)
+
     parser.add_argument('-o',
                         '--outdir',
                         help='TIF output directory',
@@ -48,6 +56,54 @@ def get_args():
                         default='bin2tif_out')
 
     return parser.parse_args()
+
+
+# --------------------------------------------------
+def get_boundingbox(metadata):
+    args = get_args()
+    z_offset = args.zoffset
+
+    with open(metadata) as f:
+        meta = json.load(f)['lemnatec_measurement_metadata']
+
+    loc_gantry_x = float(meta['sensor_fixed_metadata']['location in camera box x [m]'])
+    loc_gantry_y = float(meta['sensor_fixed_metadata']['location in camera box y [m]'])
+    loc_gantry_z = float(meta['sensor_fixed_metadata']['location in camera box z [m]'])
+
+    gantry_x = float(meta['gantry_system_variable_metadata']['position x [m]']) + loc_gantry_x
+    gantry_y = float(meta['gantry_system_variable_metadata']['position y [m]']) + loc_gantry_y
+    gantry_z = float(meta['gantry_system_variable_metadata']['position z [m]']) + z_offset + loc_gantry_z#offset in m
+
+    fov_x, fov_y = meta['sensor_fixed_metadata']['field of view X [m]'], meta['sensor_fixed_metadata']['field of view y [m]']
+
+    # img_height = int(meta['sensor_fixed_metadata']['camera resolution'].split('x')[0])
+    # img_width = int(meta['sensor_fixed_metadata']['camera resolution'].split('x')[1])
+
+    B = gantry_z
+    A_x = np.arctan((0.5*float(fov_x))/2)
+    A_y = np.arctan((0.5*float(fov_y))/2)
+    L_x = 2*B*np.tan(A_x)
+    L_y = 2*B*np.tan(A_y)
+
+    x_n = gantry_x + (L_x/2)
+    x_s = gantry_x - (L_x/2)
+    y_w = gantry_y + (L_y/2)
+    y_e = gantry_y - (L_y/2)
+
+    bbox_nw_latlon = scanalyzer_to_latlon(x_n, y_w)
+    bbox_se_latlon = scanalyzer_to_latlon(x_s, y_e)
+
+    # TERRA-REF
+    lon_shift = 0.000020308287
+
+    # Drone
+    lat_shift = 0.000018292 #0.000015258894
+    b_box =  ( bbox_se_latlon[0] - lat_shift,
+                bbox_nw_latlon[0] - lat_shift,
+                bbox_nw_latlon[1] + lon_shift,
+                bbox_se_latlon[1] + lon_shift )
+
+    return b_box
 
 
 # --------------------------------------------------
@@ -64,11 +120,7 @@ def main():
     file_endings = ["{0:0>4}.bin".format(i) for i in range(0, 101)]
 
     img_width, img_height = 1216, 1936
-
-    with open(args.metadata) as f:
-        meta = json.load(f)['content']
-        gps_bounds = geojson_to_tuples(meta['spatial_metadata']['ps2Top']['bounding_box'])
-        print(gps_bounds)
+    gps_bounds = get_boundingbox(args.metadata)
 
     png_frames = {}
     cnt = 0
